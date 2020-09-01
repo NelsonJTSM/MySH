2ac9ad53824cca4f6c928ee661b8d4bfaf9f9867
Nelson Torres <NelsonJTSM@gmail.com>
2020-08-31 23:56:44 -0400

    Added history command + -c flag

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,31 @@ enum ShellFunction {
 #[derive(Debug)]
 struct Command<'a> {
     function: ShellFunction,
+    name: &'a str,
     args: Vec<&'a str>,
 }
 
 #[derive(Debug)]
-struct ShellState {
+struct Shell {
     directory: PathBuf,
+    history: Vec<String>,
 }
 
 // ShellState handles how the shell changes states.
-impl ShellState {
+impl Shell {
     /*
     fn current_directory(&mut self) -> &mut PathBuf {
         // Assumes directory doesn't have weird chars.
         &mut self.directory
     }
     */
+
+    fn new(directory: PathBuf) -> Shell {
+        Shell {
+            directory,
+            history: Vec::new(),
+        }
+    }
 
     fn move_to_dir(&mut self, target_dir: &PathBuf) {
         self.directory = self.directory.join(target_dir);
@@ -40,12 +49,57 @@ impl ShellState {
     fn where_am_i(&self) {
         println!("{}", self.directory.display());
     }
+
+    fn add_to_history(&mut self, input: &str) {
+        self.history.push(String::from(input));
+    }
+
+    fn print_history(&self) {
+        for line in &self.history {
+            println!("{}", line);
+        }
+    }
+
+    fn run_command(&mut self, command: Command) {
+        self.add_to_history(command.name);
+
+        match command.function {
+            ShellFunction::WhereAmI => self.where_am_i(),
+            ShellFunction::MoveToDir => {
+                let target_dir = PathBuf::from(command.args.get(0).unwrap());
+                self.move_to_dir(&target_dir);
+            },
+            ShellFunction::ByeBye => {
+                std::process::exit(0);
+            },
+            ShellFunction::History => {
+                if command.args.len() == 0 {
+                    self.print_history();
+                }
+
+                // Parse -c argument to clear history.
+                match command.args.get(0) {
+                    Some(arg) => {
+                        if arg == &"-c" {
+                            println!("Clearing history...");
+                            self.history.clear();
+                        }
+                    },
+                    None => {},
+                }
+            },
+            _ => {
+                // Better command handling?
+                println!("Command not implemented yet.");
+            },
+        }
+    }
 }
 
 fn main() {
     let prompt = "# ";
 
-    let mut shell_state = start_shell_start();
+    let mut shell = start_shell_start();
 
     loop {
         print!("{}", prompt);
@@ -54,31 +108,20 @@ fn main() {
         let mut input = String::new();
 
         match io::stdin().read_line(&mut input) {
-            Ok(n) => {
+            Ok(_n) => {
                 let command = interpret_command(&input.as_str());
-                run_command(command, &mut shell_state);
+                shell.run_command(command);
             }
             Err(error) => println!("error: {}", error),
         }
     }
 }
 
-fn start_shell_start() -> ShellState {
+fn start_shell_start() -> Shell {
     // Assumes env::current_dir() is valid.
     let directory = std::env::current_dir().unwrap();
 
-    ShellState { directory }
-}
-
-fn run_command(command: Command, state: &mut ShellState) {
-    match command.function {
-        ShellFunction::WhereAmI => state.where_am_i(),
-        ShellFunction::MoveToDir => {
-            let target_dir = PathBuf::from(command.args.get(0).unwrap());
-            state.move_to_dir(&target_dir);
-        }
-        _ => {}
-    }
+    Shell::new(directory)
 }
 
 fn interpret_command(input: &str) -> Command {
@@ -86,11 +129,11 @@ fn interpret_command(input: &str) -> Command {
 
     // TODO: Handle error that occurs when no command is entered.
     // Code currently panics.
-    let cmd: &str = tokens.next().unwrap().trim_end();
+    let name: &str = tokens.next().unwrap().trim_end();
     let args: Vec<&str> = tokens.collect();
-    let function = str_to_function(cmd);
+    let function = str_to_function(name);
 
-    Command { function, args }
+    Command { function, args, name}
 }
 
 fn str_to_function(command: &str) -> ShellFunction {
